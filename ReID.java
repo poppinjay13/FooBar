42bc6213c906db3bf1e989cefc8c154321982cbf
Ian Odundo <thisian13@gmail.com>
2020-11-01 15:36:36 +0300

    re-id final commit, passed all test cases

--- ReID.java
+++ ReID.java
@@ -1,42 +1,28 @@
 public class ReID {
 
-    public static String solution(int i) {
-        if (i < 0 || i > 10000)
-            return "Please provide a valid minion ID";
-        int startIndex = i;
-        int endIndex = startIndex + 5;
-
-        String primeString = "";// initially empty
-        int realNo = 0;// loop var for holding real numbers
-        int divisor = 0;// loop var for dividing real numbers 
-
-        try {
-            for (realNo = 0; realNo <= 10000; realNo++) {
-                int factors = 0;
-                for (divisor = realNo; divisor >= 1; divisor--) {
-                    if (realNo % divisor == 0) {
-                        //check number of factors
-                        factors = factors + 1;
-                    }
-                }
-                if (factors == 2) {
-                    // if the number is prime
-                    primeString = primeString + realNo;
-                    if (primeString.length() >= endIndex)
-                        // if the lenght is greater than or equal to the endIndex
-                        break;
-                }
-            }
-            return primeString.substring(startIndex, endIndex);
-        } catch (NumberFormatException ex) {
-            return "An error occurred assigning this minion a new ID";
-        } catch (Exception ex) {
-            return "An error occurred assigning this minion a new ID";
-        }
-    }
-
     public static void main(String[] args) {
         String id = solution(5);
         System.out.println("The minion's new ID is :" + id);
     }
+
+    public static String getPrime() {
+        String rs = new String();
+        int i, j;
+        for (i = 2; i < 30000; i++) {
+            for (j = 2; j <= i; j++) {
+                if (i == j) {
+                    rs = rs + i;
+                }
+                if (i % j == 0) {
+                    break;
+                }
+            }
+        }
+        return rs;
+    }
+
+    public static String solution(int n) {
+        return getPrime().substring(n, n + 5);
+    }
+
 }
